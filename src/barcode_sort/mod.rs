1b1d87842539f5b7bb0d8daa4f59650b6630ea98
Sreenath Krishnan <sreenath.krishnan@10xgenomics.com>
2018-09-13 10:17:33 -0700

    RUS-5: Add custom sorting support to BarcodeSortWorkflow
    - And keep track of more metrics

--- src/barcode_sort/mod.rs
+++ src/barcode_sort/mod.rs
@@ -10,37 +10,42 @@ use std::path::Path;
 use serde::Serialize;
 use barcode::BarcodeChecker;
 use FastqProcessor;
+use std::borrow::Cow;
 
 // TODO: Set the parameters optimally?
 pub const SEND_BUFFER_SZ: usize = 256;
 pub const DISK_CHUNK_SZ: usize = 2048; // 2MB if ~1kB per record
 pub const ITEM_BUFFER_SZ: usize = 2097152; // 2GB if above holds
 
-pub struct BarcodeSortWorkflow<Processor, MetricType> 
+pub struct BarcodeSortWorkflow<Processor, MetricType, SortOrder> 
 where
     Processor: FastqProcessor,
     <Processor as FastqProcessor>::ReadType: 'static + HasBarcode + Send + Serialize,
     MetricType: ConfiguredReadMetric<ReadType=<Processor as FastqProcessor>::ReadType>,
+    SortOrder: SortKey<<Processor as FastqProcessor>::ReadType>,
+    <SortOrder as SortKey<<Processor as FastqProcessor>::ReadType>>::Key: 'static + Send + Ord + Serialize + Clone, 
 {
     processor: Processor,
-    sorter: BarcodeSorter<<Processor as FastqProcessor>::ReadType>,
+    sorter: BarcodeAwareSorter<<Processor as FastqProcessor>::ReadType, SortOrder>,
     checker: BarcodeChecker,
     metrics: MetricType,
     config: <MetricType as ConfiguredReadMetric>::ConfigType,
 }
 
-impl<Processor, MetricType> BarcodeSortWorkflow<Processor, MetricType>
+impl<Processor, MetricType, SortOrder> BarcodeSortWorkflow<Processor, MetricType, SortOrder>
 where
     Processor: FastqProcessor,
     <Processor as FastqProcessor>::ReadType: 'static + HasBarcode + Send + Serialize,
     MetricType: ReadMetric<ReadType=<Processor as FastqProcessor>::ReadType>,
+    SortOrder: SortKey<<Processor as FastqProcessor>::ReadType>,
+    <SortOrder as SortKey<<Processor as FastqProcessor>::ReadType>>::Key: 'static + Send + Ord + Serialize + Clone, 
 {
-    pub fn new<P: AsRef<Path>>(processor: Processor,
-        valid_path: P, 
-        invalid_path: P,
-        whitelist_path: P) -> Result<Self, Error> {
+    pub fn new(processor: Processor,
+        valid_path: impl AsRef<Path>, 
+        invalid_path: impl AsRef<Path>,
+        whitelist_path: impl AsRef<Path>) -> Result<Self, Error> {
         
-        let sorter = BarcodeSorter::new(valid_path, invalid_path)?;
+        let sorter = BarcodeAwareSorter::new(valid_path, invalid_path)?;
         let checker = BarcodeChecker::new(whitelist_path)?;
         Ok(BarcodeSortWorkflow {
             processor,
@@ -52,11 +57,13 @@ where
     }
 }
 
-impl<Processor, MetricType> BarcodeSortWorkflow<Processor, MetricType>
+impl<Processor, MetricType, SortOrder> BarcodeSortWorkflow<Processor, MetricType, SortOrder>
 where
     Processor: FastqProcessor,
     <Processor as FastqProcessor>::ReadType: 'static + HasBarcode + Send + Serialize,
     MetricType: ConfiguredReadMetric<ReadType=<Processor as FastqProcessor>::ReadType>,
+    SortOrder: SortKey<<Processor as FastqProcessor>::ReadType>,
+    <SortOrder as SortKey<<Processor as FastqProcessor>::ReadType>>::Key: 'static + Send + Ord + Serialize + Clone, 
 {
 
     pub fn new_with_config<P: AsRef<Path>>(processor: Processor,
@@ -65,7 +72,7 @@ where
         whitelist_path: P,
         config: <MetricType as ConfiguredReadMetric>::ConfigType) -> Result<Self, Error> {
         
-        let sorter = BarcodeSorter::new(valid_path, invalid_path)?;
+        let sorter = BarcodeAwareSorter::new(valid_path, invalid_path)?;
         let checker = BarcodeChecker::new(whitelist_path)?;
         Ok(BarcodeSortWorkflow {
             processor,
@@ -91,54 +98,71 @@ where
         self.sorter.finish()
     }
 
-    pub fn write_results<P: AsRef<Path>>(&self,
-        barcode_counts_path: P,
-        metrics_path: P) {
-        self.sorter.write_barcode_counts(barcode_counts_path);
-        self.metrics.to_file(metrics_path, SerdeFormat::Binary);
+    pub fn write_barcode_counts<P: AsRef<Path>>(&self, path: P) {
+        self.sorter.write_barcode_counts(path);
+    }
+
+    pub fn write_metrics<P: AsRef<Path>>(&self, metrics_path: P, format: SerdeFormat) {
+        self.metrics.to_file(metrics_path, format);
+    }
+
+    pub fn num_valid_reads(&self) -> i64 {
+        self.sorter.valid_reads
+    }
+
+    pub fn num_invalid_reads(&self) -> i64 {
+        self.sorter.invalid_reads
     }
 }
 
 pub struct BarcodeOrder;
-impl<T> SortKey<T, Barcode> for BarcodeOrder
+impl<T> SortKey<T> for BarcodeOrder
 where
     T: HasBarcode,
 {
-    fn sort_key(v: &T) -> &Barcode {
-        v.barcode()
+    type Key = Barcode;
+    fn sort_key(v: &T) -> Cow<Barcode> {
+        Cow::Borrowed(v.barcode())
     }
 }
 
-struct BarcodeSorter<T>
+struct BarcodeAwareSorter<T, Order>
 where
-    T: 'static + HasBarcode + Send + Serialize
+    T: 'static + HasBarcode + Send + Serialize,
+    Order: SortKey<T>,
 {
-    valid_writer: ShardWriter<T, Barcode, BarcodeOrder>,
+    valid_writer: ShardWriter<T, Order>,
     valid_sender: ShardSender<T>,
-    invalid_writer: ShardWriter<T, Barcode, BarcodeOrder>,
+    invalid_writer: ShardWriter<T, Order>,
     invalid_sender: ShardSender<T>,
     valid_bc_distribution: SimpleHistogram<Barcode>,
+    valid_reads: i64,
+    invalid_reads: i64,
 }
 
-impl<T> BarcodeSorter<T>
+impl<T, Order> BarcodeAwareSorter<T, Order>
 where
-    T: 'static + HasBarcode + Send + Serialize
+    T: 'static + HasBarcode + Send + Serialize,
+    Order: SortKey<T>,
+    <Order as SortKey<T>>::Key: 'static + Send + Ord + Serialize + Clone, 
 {
-    fn new<P: AsRef<Path>>(valid_path: P, invalid_path: P) -> Result<Self, Error> {
-        let valid_writer: ShardWriter<T, Barcode, BarcodeOrder> = 
+    fn new(valid_path: impl AsRef<Path>, invalid_path: impl AsRef<Path>) -> Result<Self, Error> {
+        let valid_writer: ShardWriter<T, Order> = 
             ShardWriter::new(valid_path, SEND_BUFFER_SZ, DISK_CHUNK_SZ, ITEM_BUFFER_SZ)?;
         let valid_sender = valid_writer.get_sender();
 
-        let invalid_writer: ShardWriter<T, Barcode, BarcodeOrder> = 
+        let invalid_writer: ShardWriter<T, Order> = 
             ShardWriter::new(invalid_path, SEND_BUFFER_SZ, DISK_CHUNK_SZ, ITEM_BUFFER_SZ)?;
         let invalid_sender = invalid_writer.get_sender();
 
-        Ok(BarcodeSorter {
+        Ok(BarcodeAwareSorter {
             valid_writer,
             valid_sender,
             invalid_writer,
             invalid_sender,
             valid_bc_distribution: SimpleHistogram::new(),
+            valid_reads: 0,
+            invalid_reads: 0,
         })
     }
 
@@ -146,8 +170,10 @@ where
         if read.barcode().is_valid() {
             self.valid_bc_distribution.observe(read.barcode().clone());
             self.valid_sender.send(read);
+            self.valid_reads += 1;
         } else {
             self.invalid_sender.send(read);
+            self.invalid_reads += 1;
         }
     }
 
