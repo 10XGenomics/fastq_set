61f1dc9c1f7144c66f6558cb8c320d44e962fe54
Sreenath Krishnan <sreenath.krishnan@10xgenomics.com>
2018-11-14 17:21:56 -0800

    Add polyA fasta files for testing

--- tests/run_cutadapt.sh
+++ tests/run_cutadapt.sh
@@ -5,3 +5,10 @@ cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC$ -o cutadapt_v${v}_anchored_3p.fa
 cutadapt -g AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -o cutadapt_v${v}_anywhere_5p.fa --overlap 5 input.fa > /dev/null
 cutadapt -g XAGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -o cutadapt_v${v}_non_internal_5p.fa --overlap 5 input.fa > /dev/null
 cutadapt -g ^AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -o cutadapt_v${v}_anchored_5p.fa --overlap 5 input.fa > /dev/null
+
+cutadapt -a AAAAAAAAAAAAAAAAAAAA -o cutadapt_v${v}_anywhere_3p_polyA.fa --overlap 5 input_polyA.fa > /dev/null
+cutadapt -a AAAAAAAAAAAAAAAAAAAAX -o cutadapt_v${v}_non_internal_3p_polyA.fa --overlap 5 input_polyA.fa > /dev/null
+cutadapt -a AAAAAAAAAAAAAAAAAAAA$ -o cutadapt_v${v}_anchored_3p_polyA.fa --overlap 5 input_polyA.fa > /dev/null
+cutadapt -g AAAAAAAAAAAAAAAAAAAA -o cutadapt_v${v}_anywhere_5p_polyA.fa --overlap 5 input_polyA.fa > /dev/null
+cutadapt -g XAAAAAAAAAAAAAAAAAAAA -o cutadapt_v${v}_non_internal_5p_polyA.fa --overlap 5 input_polyA.fa > /dev/null
+cutadapt -g ^AAAAAAAAAAAAAAAAAAAA -o cutadapt_v${v}_anchored_5p_polyA.fa --overlap 5 input_polyA.fa > /dev/null
