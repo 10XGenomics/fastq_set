b9692eadea9b831aaf868fa999f6d7aff4835a9f
Nigel Delaney <evolvedmicrobe@users.noreply.github.com>
2022-04-28 14:45:17 -0700

    Increase Max Valid QV Value to 93
    Newer sequencers (e.g. PacB HiFi) can produce QV values up to 93.  Although these QV values are equivalent to absurdly small error rates that likely are not estimated accurately (e.g. QV 92 is an error rate of 6.309573e-10) as the wikipedia article for FASTQ file format states: "Since the maximum observed quality score was previously only 40, various scripts and tools break when they encounter data with quality values larger than 40. For processed reads, scores may be even higher. For example, quality values of 45 are observed in reads from Illumina's Long Read Sequencing Service (previously Moleculo)."
    
    As such, this PR accepts anything up to the maximum allowed value.

--- src/squality.rs
+++ src/squality.rs
@@ -15,7 +15,7 @@ impl ArrayContent for SQualityContents {
     fn validate_bytes(seq: &[u8]) {
         for (i, &c) in seq.iter().enumerate() {
             let q = c as i16 - 33;
-            if !(0..42).contains(&q) {
+            if !(0..94).contains(&q) {
                 panic!(
                     "Invalid quality value {} ASCII character {} at position {}",
                     q, c, i
