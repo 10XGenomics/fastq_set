523788370cd783d850bb104662d03858c458503a
Sreenath Krishnan <sreenath.krishnan@10xgenomics.com>
2023-03-15 22:55:55 -0700

    Don't leave non-zero in unused space in the buffer

--- src/array.rs
+++ src/array.rs
@@ -54,6 +54,7 @@ where
     pub fn remove(&mut self, pos: usize) -> u8 {
         let val = self[pos];
         self.as_mut_bytes().split_at_mut(pos).1.rotate_left(1);
+        self.bytes[N - 1] = 0;
         self.length -= 1;
         val
     }
